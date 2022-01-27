6769be44e14d00b4e2ad25250da4c8ab402c3df8
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2022-01-27 16:38:50 +0300

    update deps

--- sleep.js
+++ sleep.js
@@ -1,12 +1,11 @@
-const process = require('process');
 
 const shutdown = message => {
     console.log(message);
     process.exit(0);
-}
+};
 
 process.on('SIGINT',  () => shutdown('Exit on SIGINT'));
 process.on('SIGTERM', () => shutdown('Exit on SIGTERM'));
 
-const sleep = async () => { while(1) await new Promise(resolve => setTimeout(_ => resolve(), 3600000)); };
-sleep();
+const fn = timeout => setTimeout(fn, timeout, timeout);
+fn(3600000);
